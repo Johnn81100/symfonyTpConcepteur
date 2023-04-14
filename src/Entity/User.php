5868787535863c1da5d52b1a1a42a88b902af887
Johnn81100 <103576060+Johnn81100@users.noreply.github.com>
2023-04-14 17:03:57 +0200

    feat():  ajout route   dans le controller Article Ajout  méthode magique dans les Entités Categorie et User Suppression du contenu  du template home/index.html.twig Ajout lien dans le template header mis à jour template article/index2 Création formulaire Article Création templates pour la liste des articles  et  ajouter un article

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -44,7 +44,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
     public function __toString()
     {
-        return $this->nom.' '.$this->prenom;
+        return $this->nom;
     }
 
     public function getId(): ?int
